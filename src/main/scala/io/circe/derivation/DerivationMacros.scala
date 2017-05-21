49917afd453a721153c11a3f1d63dd807dd136f0
Travis Brown <travisrobertbrown@gmail.com>
2017-05-21 11:41:44 -0500

    Simplification

--- src/main/scala/io/circe/derivation/DerivationMacros.scala
+++ src/main/scala/io/circe/derivation/DerivationMacros.scala
@@ -6,37 +6,41 @@ import scala.reflect.macros.blackbox
 class DerivationMacros(val c: blackbox.Context) {
   import c.universe._
 
-  def fail(tpe: Type): Nothing = c.abort(c.enclosingPosition, s"Could not identify primary constructor for $tpe")
+  private[this] case class Member(name: Name, decodedName: String, tpe: Type)
+
+  private[this] def fail(tpe: Type): Nothing = c.abort(
+    c.enclosingPosition,
+    s"Could not identify primary constructor for $tpe"
+  )
+
+  private[this] def membersFromPrimaryConstr(tpe: Type): Option[List[Member]] = tpe.decls.collectFirst {
+    case m: MethodSymbol if m.isPrimaryConstructor => m.paramLists.flatten.map { field =>
+      Member(field.name, field.name.decodedName.toString, tpe.decl(field.name).typeSignature)
+    }
+  }
 
   def materializeDecoderImpl[T: c.WeakTypeTag]: c.Expr[Decoder[T]] = {
     val tpe = weakTypeOf[T]
 
-    val primaryConstructor = tpe.decls.collectFirst {
-      case m: MethodSymbol if m.isPrimaryConstructor => m
-    }
-
-    primaryConstructor.fold(fail(tpe)) { constructor =>
-      val fieldNames: List[Name] = constructor.paramLists.flatten.map(_.name)
-      val decodedNames: List[String] = fieldNames.map(_.decodedName.toString)
-      val fieldTypes: List[Type] = constructor.paramLists.flatten.map { field =>
-        tpe.decl(field.name).typeSignature
-      }
-      val fieldCount = fieldNames.size
-      val functionParameters = fieldNames.zip(fieldTypes).map {
-        case (fieldName, fieldType) =>
+    membersFromPrimaryConstr(tpe).fold(fail(tpe)) { members =>
+      val fieldCount = members.size
+      val functionParameters = members.map {
+        case Member(fieldName, _, fieldType) =>
           val termName = TermName(fieldName.toString)
           q"$termName: $fieldType"
       }
-      val parameters = fieldNames.map { fieldName =>
-        val termName = TermName(fieldName.toString)
-        q"$termName"
+
+      val parameters = members.map {
+        case Member(fieldName, _, _) =>
+          val termName = TermName(fieldName.toString)
+          q"$termName"
       }
 
       val methodName = TermName(s"forProduct$fieldCount")
 
       if (fieldCount > 0) c.Expr[Decoder[T]](
         q"""
-          _root_.io.circe.Decoder.$methodName[..$fieldTypes, $tpe](..$decodedNames)(
+          _root_.io.circe.Decoder.$methodName[..${ members.map(_.tpe) }, $tpe](..${ members.map(_.decodedName) })(
             (..$functionParameters) => new $tpe(..$parameters)
           )
         """
@@ -47,27 +51,19 @@ class DerivationMacros(val c: blackbox.Context) {
   def materializeEncoderImpl[T: c.WeakTypeTag]: c.Expr[ObjectEncoder[T]] = {
     val tpe = weakTypeOf[T]
 
-    val primaryConstructor = tpe.decls.collectFirst {
-      case m: MethodSymbol if m.isPrimaryConstructor => m
-    }
-
-    primaryConstructor.fold(fail(tpe)) { constructor =>
-      val fieldNames: List[Name] = constructor.paramLists.flatten.map(_.name)
-      val decodedNames: List[String] = fieldNames.map(_.decodedName.toString)
-      val fieldTypes: List[Type] = constructor.paramLists.flatten.map { field =>
-        tpe.decl(field.name).typeSignature
-      }
-      val fieldCount = fieldNames.size
-      val invocations = fieldNames.map { fieldName => 
-        val termName = TermName(fieldName.toString)
-        q"toEncode.$termName"
+    membersFromPrimaryConstr(tpe).fold(fail(tpe)) { members =>
+      val fieldCount = members.size
+      val invocations = members.map {
+        case Member(fieldName, _, _) =>
+          val termName = TermName(fieldName.toString)
+          q"toEncode.$termName"
       }
 
       val methodName = TermName(s"forProduct$fieldCount")
 
       if (fieldCount > 0) c.Expr[ObjectEncoder[T]](
         q"""
-          _root_.io.circe.Encoder.$methodName[..$fieldTypes, $tpe](..$decodedNames)(
+          _root_.io.circe.Encoder.$methodName[..${ members.map(_.tpe) }, $tpe](..${ members.map(_.decodedName) })(
             toEncode => (..$invocations)
           )
         """
