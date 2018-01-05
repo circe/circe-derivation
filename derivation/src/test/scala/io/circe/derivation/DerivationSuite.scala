b1a67c287b4601903b9427bbe028010c4af95f2a
Travis Brown <travisrobertbrown@gmail.com>
2018-01-05 13:32:20 -0600

    Add support for multi-parameter constructors

--- derivation/src/test/scala/io/circe/derivation/DerivationSuite.scala
+++ derivation/src/test/scala/io/circe/derivation/DerivationSuite.scala
@@ -1,7 +1,7 @@
 package io.circe.derivation
 
 import io.circe.{ Decoder, Encoder, ObjectEncoder }
-import io.circe.examples.{ Bar, Baz, Foo, Qux }
+import io.circe.examples.{ Bar, Baz, Foo, MultiParamListClass, Qux, SimpleClass }
 import io.circe.testing.CodecTests
 
 object DerivationSuiteCodecs extends Serializable {
@@ -13,6 +13,11 @@ object DerivationSuiteCodecs extends Serializable {
   implicit val encodeBaz: ObjectEncoder[Baz] = deriveEncoder
   implicit def decodeQux[A: Decoder]: Decoder[Qux[A]] = deriveDecoder
   implicit def encodeQux[A: Encoder]: ObjectEncoder[Qux[A]] = deriveEncoder
+
+  implicit val decodeMultiParamListClass: Decoder[MultiParamListClass] = deriveDecoder
+  implicit val encodeMultiParamListClass: Encoder[MultiParamListClass] = deriveEncoder
+  implicit val decodeSimpleClass: Decoder[SimpleClass] = deriveDecoder
+  implicit val encodeSimpleClass: Encoder[SimpleClass] = deriveEncoder
 }
 
 class DerivationSuite extends CirceSuite {
@@ -22,4 +27,7 @@ class DerivationSuite extends CirceSuite {
   checkLaws("Codec[Bar]", CodecTests[Bar].codec)
   checkLaws("Codec[Baz]", CodecTests[Baz].codec)
   checkLaws("Codec[Qux[Baz]]", CodecTests[Qux[Baz]].codec)
+
+  checkLaws("Codec[MultiParamListClass]", CodecTests[MultiParamListClass].codec)
+  checkLaws("Codec[SimpleClass]", CodecTests[SimpleClass].codec)
 }
