de057f1fc6c6585ffc5c93aa08204c594c5cd34b
Travis Brown <travisrobertbrown@gmail.com>
2018-03-13 13:55:17 -0500

    Fix whitespace

--- examples/scrooge/src/main/scala/io/circe/examples/scrooge/package.scala
+++ examples/scrooge/src/main/scala/io/circe/examples/scrooge/package.scala
@@ -5,19 +5,19 @@ import org.scalacheck.Arbitrary
 
 package object scrooge {
   implicit val arbitrarySomethingStruct: Arbitrary[SomethingStruct] = Arbitrary(
-  	for {
-  	  a <- Arbitrary.arbitrary[String]
-  	  b <- Arbitrary.arbitrary[Option[Long]]
-  	  c <- Arbitrary.arbitrary[List[String]]
-  	} yield SomethingStruct(a, b, c)
+    for {
+      a <- Arbitrary.arbitrary[String]
+      b <- Arbitrary.arbitrary[Option[Long]]
+      c <- Arbitrary.arbitrary[List[String]]
+    } yield SomethingStruct(a, b, c)
   )
   implicit val eqSomethingStruct: Eq[SomethingStruct] = Eq.fromUniversalEquals
 
   implicit val arbitraryBiggerStruct: Arbitrary[BiggerStruct] = Arbitrary(
-  	for {
-  	  d <- Arbitrary.arbitrary[SomethingStruct]
-  	  e <- Arbitrary.arbitrary[Option[String]]
-  	} yield BiggerStruct(d, e)
+    for {
+      d <- Arbitrary.arbitrary[SomethingStruct]
+      e <- Arbitrary.arbitrary[Option[String]]
+    } yield BiggerStruct(d, e)
   )
   implicit val eqBiggerStruct: Eq[BiggerStruct] = Eq.fromUniversalEquals
 }
