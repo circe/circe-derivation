7bc0a0e12e90c758843df580a95e52550e2c11a9
Travis Brown <travisrobertbrown@gmail.com>
2018-03-13 13:55:32 -0500

    Add derivations

--- examples/derivation/jvm/src/main/scala/io/circe/examples/derivation/ScroogeInstances.scala
+++ examples/derivation/jvm/src/main/scala/io/circe/examples/derivation/ScroogeInstances.scala
@@ -9,4 +9,39 @@ object ScroogeInstances {
   implicit val encodeSomethingStruct: ObjectEncoder[SomethingStruct] = deriveEncoder
   implicit val decodeBiggerStruct: Decoder[BiggerStruct] = deriveDecoder
   implicit val encodeBiggerStruct: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct1: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct1: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct1: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct1: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct2: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct2: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct2: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct2: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct3: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct3: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct3: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct3: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct4: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct4: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct4: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct4: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct5: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct5: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct5: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct5: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct6: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct6: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct6: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct6: ObjectEncoder[BiggerStruct] = deriveEncoder
+
+  val decodeSomethingStruct7: Decoder[SomethingStruct] = deriveDecoder
+  val encodeSomethingStruct7: ObjectEncoder[SomethingStruct] = deriveEncoder
+  val decodeBiggerStruct7: Decoder[BiggerStruct] = deriveDecoder
+  val encodeBiggerStruct7: ObjectEncoder[BiggerStruct] = deriveEncoder
 }
