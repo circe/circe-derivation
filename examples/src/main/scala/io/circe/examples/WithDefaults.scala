b7c61223e63b4da0d1d4dd8463524549dd07826d
Travis Brown <travisrobertbrown@gmail.com>
2019-08-30 02:42:30 -0500

    Fix useDefaults, etc.

---
+++ examples/src/main/scala/io/circe/examples/WithDefaults.scala
@@ -0,0 +1,18 @@
+package io.circe.examples
+
+import cats.kernel.Eq
+import org.scalacheck.Arbitrary
+
+case class WithDefaults(i: Int, j: Int = 1, k: List[String] = List(""))
+
+object WithDefaults {
+	implicit val arbitraryWithDefaults: Arbitrary[WithDefaults] = Arbitrary(
+    for {
+      i <- Arbitrary.arbitrary[Int]
+      j <- Arbitrary.arbitrary[Int]
+      k <- Arbitrary.arbitrary[List[String]]
+    } yield WithDefaults(i, j, k)
+  )
+
+  implicit val eqWithDefaults: Eq[WithDefaults] = Eq.fromUniversalEquals
+}
