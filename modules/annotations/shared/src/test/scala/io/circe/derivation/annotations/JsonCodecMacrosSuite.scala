0c4bf3d8add0fafb78627dc7d347106f261f94ab
Travis Brown <travisrobertbrown@gmail.com>
2019-10-10 05:32:28 -0500

    Fix test

--- modules/annotations/shared/src/test/scala/io/circe/derivation/annotations/JsonCodecMacrosSuite.scala
+++ modules/annotations/shared/src/test/scala/io/circe/derivation/annotations/JsonCodecMacrosSuite.scala
@@ -108,11 +108,10 @@ package jsoncodecmacrossuiteaux {
     implicit val eqSelfRecursiveWithOption: Eq[SelfRecursiveWithOption] = Eq.fromUniversalEquals
 
     private def atDepth(depth: Int): Gen[SelfRecursiveWithOption] = if (depth < 3)
-      Arbitrary
-        .arbitrary[Option[SelfRecursiveWithOption]]
-        .map(
-          SelfRecursiveWithOption(_)
-        )
+      Gen.oneOf(
+        Gen.const(SelfRecursiveWithOption(None)),
+        atDepth(depth + 1).map(x => SelfRecursiveWithOption(Some(x)))
+      )
     else Gen.const(SelfRecursiveWithOption(None))
 
     implicit val arbitrarySelfRecursiveWithOption: Arbitrary[SelfRecursiveWithOption] =
