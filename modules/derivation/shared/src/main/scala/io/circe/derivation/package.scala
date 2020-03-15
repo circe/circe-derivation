b902e9ddd0ea0f43711b3a5dc12604012ddcf8ed
shrynx <shriyansbhatnagar@gmail.com>
2020-03-15 20:00:02 +0100

    added default arguments for decoder/encoder with transformNames

--- modules/derivation/shared/src/main/scala/io/circe/derivation/package.scala
+++ modules/derivation/shared/src/main/scala/io/circe/derivation/package.scala
@@ -14,11 +14,17 @@ package object derivation {
   ): Decoder[A] =
     macro DerivationMacros.materializeDecoderWithTransformNames[A]
 
+  final def deriveDecoder[A](transformNames: String => String): Decoder[A] =
+    macro DerivationMacros.materializeDecoderWithTransformNamesAndDefaults[A]
+
   final def deriveEncoder[A](
     transformNames: String => String,
     discriminator: Option[String]
   ): Encoder.AsObject[A] =
     macro DerivationMacros.materializeEncoderWithTransformNames[A]
+
+  final def deriveEncoder[A](transformNames: String => String): Encoder.AsObject[A] =
+    macro DerivationMacros.materializeEncoderWithTransformNamesAndDefaults[A]
 
   final def deriveCodec[A](
     transformNames: String => String,
@@ -26,4 +32,9 @@ package object derivation {
     discriminator: Option[String]
   ): Codec.AsObject[A] =
     macro DerivationMacros.materializeCodecWithTransformNames[A]
+
+  final def deriveCodec[A](
+    transformNames: String => String
+  ): Codec.AsObject[A] =
+    macro DerivationMacros.materializeCodecWithTransformNamesAndDefaults[A]
 }
